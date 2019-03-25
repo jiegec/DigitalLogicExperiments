0af62c82986882f35f5cbba2bc717e80ebb0acf8
Jiajie Chen <jiegec@qq.com>
2019-03-25 17:19:10 +0800

    First working version

--- DigitalLife.sv
+++ DigitalLife.sv
@@ -1,15 +1,19 @@
 module DigitalLife (
 	input wire clk,
-	input wire rst_n,
-	output reg count[4:0]
+	input wire rst,
+	output reg [3:0] count
 );
 
 	always_ff @ (posedge clk) begin
-		if (rst_n == '0') begin
-			count = 4b'0000;
-		else
-			count = count + 1;
+		if (rst == 1'b1) begin
+			count <= 4'b0000;
+		end else begin
+		   if (count <= 4'b1000) begin
+				count <= count + 4'b1;
+			end else begin
+				count <= 0;
+			end
 		end
 	end
 
-end module // DigitalLife
+endmodule // DigitalLife
